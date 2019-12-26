156bf5a5cc1603adc0f27561abb22233b8f2491d
prasadchoudhari819 <prasadchoudhari819@gmail.com>
2019-12-26 20:01:42 +0530

    add index file and calculation

--- download.js
+++ download.js
@@ -1,6 +1,11 @@
-console.log('Hello World');
+function add (p1, p2){
+   return p1 + p2;
+}
+// My command
+console.log(add(12, 23));
 
-console.log('Hello, this is my test message');
+function subtract (s1, s2){
+    return s1 - s2;
+}
 
-
-console.log('this is second test');
+console.log(subtract(15, 5));
